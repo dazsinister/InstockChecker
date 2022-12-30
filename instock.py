8be1ef06564fe3457b8c5f3cac4074a32e58922f
Joshua McDermott <35087894+dazsinister@users.noreply.github.com>
2022-12-30 14:07:20 -0500

    Update instock.py

--- instock.py
+++ instock.py
@@ -5,11 +5,8 @@ from discord import SyncWebhook
 
 url = "https://www.instocktrades.com/products/mar221128d/captain-marvel-by-kelly-sue-deconnick-omnibus-hc-lopez-var"
 url2 = "https://www.instocktrades.com/products/mar221129d/captain-marvel-by-kelly-sue-deconnick-omnibus-hc-mckelvie-dm"
-url3 = "https://www.instocktrades.com/products/mar221138d/predator-orig-yrs-omnibus-hc-vol-01-coello-cvr"
-url4 = "https://www.instocktrades.com/products/mar221139d/predator-orig-yrs-omnibus-hc-vol-01-warner-dm-var"
-url5 = "https://www.instocktrades.com/products/aug227229d/sleeper-omnibus-hc-(2022-edition)-(mr)"
-url6 = "https://www.instocktrades.com/products/jul220165d/spawn-origins-dlx-ed-hc-vol-05"
-
+url3 = "https://www.instocktrades.com/products/APR221051d/APR221051"
+url4 = "https://www.instocktrades.com/products/APR221050d/APR221050"
 
 def is_in_stock(url):
     result = requests.get(url)
@@ -38,7 +35,7 @@ while True:
     if is_in_stock(url3):
         webhook = SyncWebhook.from_url(
             "https://discord.com/api/webhooks/1052223549563215962/W45EREAJfU7xYI7KJ_v7o6y5FCqDwMaY6DNtZgHGzZWbMmma8Kf5lRYThKflXMSEdKKt")
-        webhook.send("Predator Original years Omnibus Volume 01")
+        webhook.send("AVENGERS BY BUSIEK & PEREZ OMNIBUS VOL. 1 HC PEREZ ANNIVERSARY COVER")
         webhook.send(
             "https://www.instocktrades.com/products/mar221138d/predator-orig-yrs-omnibus-hc-vol-01-coello-cvr")
         webhook.send("__")
@@ -46,25 +43,9 @@ while True:
     if is_in_stock(url4):
         webhook = SyncWebhook.from_url(
             "https://discord.com/api/webhooks/1052223549563215962/W45EREAJfU7xYI7KJ_v7o6y5FCqDwMaY6DNtZgHGzZWbMmma8Kf5lRYThKflXMSEdKKt")
-        webhook.send("Predator Original Years Omnibus Volume 01 Warner DM Variant")
+        webhook.send("AVENGERS BY BUSIEK & PEREZ OMNIBUS VOL. 1 HC PEREZ FIRST ISSUE COVER")
         webhook.send(
             "https://www.instocktrades.com/products/mar221139d/predator-orig-yrs-omnibus-hc-vol-01-warner-dm-var")
-        webhook.send("__")
-
-    if is_in_stock(url5):
-        webhook = SyncWebhook.from_url(
-            "https://discord.com/api/webhooks/1052223549563215962/W45EREAJfU7xYI7KJ_v7o6y5FCqDwMaY6DNtZgHGzZWbMmma8Kf5lRYThKflXMSEdKKt")
-        webhook.send("Sleeper Omnibus 2022 edition")
-        webhook.send(
-            "https://www.instocktrades.com/products/aug227229d/sleeper-omnibus-hc-(2022-edition)-(mr)")
-        webhook.send("__")
-
-    if is_in_stock(url6):
-        webhook = SyncWebhook.from_url(
-            "https://discord.com/api/webhooks/1052223549563215962/W45EREAJfU7xYI7KJ_v7o6y5FCqDwMaY6DNtZgHGzZWbMmma8Kf5lRYThKflXMSEdKKt")
-        webhook.send("Spawn Origins Deluxe Edition volume 05")
-        webhook.send(
-            "https://www.instocktrades.com/products/jul220165d/spawn-origins-dlx-ed-hc-vol-05")
         webhook.send("__")
 
     time.sleep(0)
