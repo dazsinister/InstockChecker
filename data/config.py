af5126ba4dd7493615c2e7e73c47ed5811658779
Joshua McDermott <35087894+dazsinister@users.noreply.github.com>
2023-01-26 13:01:13 -0500

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,20 @@
+link = ""
+
+BOTTOKEN = ""
+
+#email stuff
+send = ''
+receive = ''
+password = ''
+
+#url for everything we want to search for
+url1 = ""
+url2 = ""
+url3 = ""
+url4 = ""
+url5 = ""
+url6 = ""
+url7 = ""
+url8 = ""
+url9 = ""
+url10 = ""
